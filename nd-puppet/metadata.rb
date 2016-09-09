3d86621e9047367f1d7d30083e31e5c5c2218766
Matt Wise <matt@nextdoor.com>
2016-09-09 11:10:02 -0700

    Force the LANGg

--- nd-puppet/metadata.rb
+++ nd-puppet/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@nextdoor.com'
 license          'Apache 2.0'
 description      'Installs/Configures Puppet'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.17'
+version          '0.1.18'
 
 depends "marker"
 depends "machine_tag"
